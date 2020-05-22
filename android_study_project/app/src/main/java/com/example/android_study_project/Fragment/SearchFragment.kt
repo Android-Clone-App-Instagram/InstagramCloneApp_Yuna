8165459b7c93fe6947f8fb9c0a7a1c8fc6614eb7
yunakim2 <98_02@naver.com>
2020-05-22 15:15:02 +0900

    안드로이드 스터디 _ BottomNavigationBar + Fragment

---
+++ android_study_project/app/src/main/java/com/example/android_study_project/Fragment/SearchFragment.kt
@@ -0,0 +1,24 @@
+package com.example.android_study_project.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.android_study_project.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+}
